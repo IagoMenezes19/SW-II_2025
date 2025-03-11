5395854af8215987dec8b79fb4435c0058b50881
IagoMenezes19 <iagomenez1906@gmail.com>
2025-03-11 08:40:10 -0300

    i dentro da variavel

---
+++ Aula03/exemplo.php
@@ -0,0 +1,13 @@
+<?php
+    function gera_aleatorio(){
+        $vetor = array();
+        for($i = 0; $i <= 9; $i++){
+            rand(0,9);
+            $vetor[$i] = rand(0,9);
+        }  
+        return $vetor;
+    }
+
+    $recebe_vetor = gera_aleatorio();
+    print_r($recebe_vetor);
+?>
